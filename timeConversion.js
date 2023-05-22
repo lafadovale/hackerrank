6d434928a8e344bcaca194b3589dfb35d9563856
Lafaiete do Vale <lafa.vale@gmail.com>
2023-05-22 02:11:29 -0300

    Time Conversion solved

---
+++ timeConversion.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(time) {
+  let hours = parseInt(time.substring(0, 2));
+  let minutes = parseInt(time.substring(3, 5));
+  let seconds = parseInt(time.substring(6, 8));
+  let period = time.substring(8);
+
+  if (hours == 12) {
+    hours = 0;
+  }
+
+  if (period === "PM") {
+    hours += 12;
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = readLine();
+
+  const result = timeConversion(s);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
