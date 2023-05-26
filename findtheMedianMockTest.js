4e9b44dc2a016039945e9afa88f6e7ad02620499
Lafaiete do Vale <lafa.vale@gmail.com>
2023-05-25 23:44:27 -0300

    Find the Median with even case

--- findtheMedianMockTest.js
+++ findtheMedianMockTest.js
@@ -1,9 +1,15 @@
 const arr = [1, 3, 4, 5, 7, 2, 6];
 
-// only if the median is an odd number
-function arrMedium(arr) {
-  const sorted = arr.sort((a, b) => a - b);
-  console.log(sorted[Math.floor(sorted.length / 2)]);
+function findMedian(arr) {
+  arr.sort((a, b) => a - b);
+  let middleIndex = Math.floor(arr.length / 2);
+  // if the median is an odd number
+  if (arr.length % 2 !== 0) {
+    console.log(arr[middleIndex / 2]);
+  } else {
+    // if the median is an even number
+    console.log((arr[middleIndex] + arr[middleIndex - 1]) / 2);
+  }
 }
 
-arrMedium(arr);
+findMedian(arr);
