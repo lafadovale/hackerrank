9aefb6bff360c59bd206d72e8d9e5def1d744e3e
lafadovale <lafa.vale@gmail.com>
2023-06-03 02:38:14 -0300

    Prettier formatting

--- leftRotation.js
+++ leftRotation.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,30 +32,33 @@ function readLine() {
  */
 
 function rotateLeft(d, arr) {
-    // Write your code here
-    let number = 0
-    for (let i = 0; i < d; i++) {
-        number = arr[0]
-        arr.shift()
-        arr.push(number)
-    }
-    return arr
+  // Write your code here
+  let number = 0;
+  for (let i = 0; i < d; i++) {
+    number = arr[0];
+    arr.shift();
+    arr.push(number);
+  }
+  return arr;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+  const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
 
-    const n = parseInt(firstMultipleInput[0], 10);
+  const n = parseInt(firstMultipleInput[0], 10);
 
-    const d = parseInt(firstMultipleInput[1], 10);
+  const d = parseInt(firstMultipleInput[1], 10);
 
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
 
-    const result = rotateLeft(d, arr);
+  const result = rotateLeft(d, arr);
 
-    ws.write(result.join(' ') + '\n');
+  ws.write(result.join(" ") + "\n");
 
-    ws.end();
+  ws.end();
 }
