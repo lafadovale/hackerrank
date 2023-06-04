73281be5d8b77fc3598c70691af135002b284697
lafadovale <lafa.vale@gmail.com>
2023-06-04 20:55:49 -0300

    Adjustment

--- countingSort1.js
+++ countingSort1.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'countingSort' function below.
  *
@@ -36,21 +12,4 @@ function countingSort(arr) {
     freqArr[arr[i]] += 1;
   }
   return freqArr;
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const n = parseInt(readLine().trim(), 10);
-
-  const arr = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((arrTemp) => parseInt(arrTemp, 10));
-
-  const result = countingSort(arr);
-
-  ws.write(result.join(" ") + "\n");
-
-  ws.end();
 }
