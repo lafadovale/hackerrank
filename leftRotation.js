588f9855fa28765a637d425cc9ebefe6e673aa8f
lafadovale <lafa.vale@gmail.com>
2023-06-05 01:02:47 -0300

    Code adjustment

--- leftRotation.js
+++ leftRotation.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'rotateLeft' function below.
  *
@@ -40,25 +16,4 @@ function rotateLeft(d, arr) {
     arr.push(number);
   }
   return arr;
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
-
-  const n = parseInt(firstMultipleInput[0], 10);
-
-  const d = parseInt(firstMultipleInput[1], 10);
-
-  const arr = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((arrTemp) => parseInt(arrTemp, 10));
-
-  const result = rotateLeft(d, arr);
-
-  ws.write(result.join(" ") + "\n");
-
-  ws.end();
 }
