a01dd0e814862bed2157a19b5d7f3c9a3c4530ae
lafadovale <lafa.vale@gmail.com>
2023-06-06 20:34:32 -0300

    Solution isolated from the rest

--- maximumPerimeterTriangle.js
+++ maximumPerimeterTriangle.js
@@ -1,27 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
 /*
  * Complete the 'maximumPerimeterTriangle' function below.
  *
@@ -30,36 +6,22 @@ function readLine() {
  */
 
 function maximumPerimeterTriangle(sticks) {
-    // Write your code here
-    const arr = sticks.sort((a, b) => a - b)
-    let answer = 0
-    let result = []
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            for (let k = j + 1; k < arr.length; k++) {
-                let current = 0
-                if (arr[i] + arr[j] > arr[k]) current = arr[i] + arr[j] + arr[k] // condition to be a triangle
-                if (current > ans) {
-                    answer = current
-                    result = [arr[i], arr[j], arr[k]]
-                }
-            }
+  // Write your code here
+  const arr = sticks.sort((a, b) => a - b);
+  let answer = 0;
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        let current = 0;
+        if (arr[i] + arr[j] > arr[k]) current = arr[i] + arr[j] + arr[k]; // condition to be a triangle
+        if (current > ans) {
+          answer = current;
+          result = [arr[i], arr[j], arr[k]];
         }
+      }
     }
-    if (answer === 0) return [-1]
-    return result
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const n = parseInt(readLine().trim(), 10);
-
-    const sticks = readLine().replace(/\s+$/g, '').split(' ').map(sticksTemp => parseInt(sticksTemp, 10));
-
-    const result = maximumPerimeterTriangle(sticks);
-
-    ws.write(result.join(' ') + '\n');
-
-    ws.end();
+  }
+  if (answer === 0) return [-1];
+  return result;
 }
