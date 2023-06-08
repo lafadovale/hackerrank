8a7adff2608a7d3453dcc40c6076fa12fdf092f6
lafadovale <lafa.vale@gmail.com>
2023-06-07 22:06:20 -0300

    Solution isolated

--- pangrams.js
+++ pangrams.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'pangrams' function below.
  *
@@ -35,16 +11,4 @@ function pangrams(s) {
   const regex = /[a-z]/g;
   const uniqueLetters = [...new Set(lowerCasePhrase.match(regex))];
   return uniqueLetters.length === 26 ? "pangram" : "not pangram";
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const s = readLine();
-
-  const result = pangrams(s);
-
-  ws.write(result + "\n");
-
-  ws.end();
 }
