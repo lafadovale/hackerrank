40b39a4f562acb4046baf4d10e0e87d46220099c
lafadovale <lafa.vale@gmail.com>
2023-06-07 22:06:27 -0300

    Solution isolated

--- permutingTwoArrays.js
+++ permutingTwoArrays.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'twoArrays' function below.
  *
@@ -40,34 +16,4 @@ function twoArrays(k, A, B) {
     if (A[i] + B[i] < k) return "NO";
   }
   return "YES";
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const q = parseInt(readLine().trim(), 10);
-
-  for (let qItr = 0; qItr < q; qItr++) {
-    const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
-
-    const n = parseInt(firstMultipleInput[0], 10);
-
-    const k = parseInt(firstMultipleInput[1], 10);
-
-    const A = readLine()
-      .replace(/\s+$/g, "")
-      .split(" ")
-      .map((ATemp) => parseInt(ATemp, 10));
-
-    const B = readLine()
-      .replace(/\s+$/g, "")
-      .split(" ")
-      .map((BTemp) => parseInt(BTemp, 10));
-
-    const result = twoArrays(k, A, B);
-
-    ws.write(result + "\n");
-  }
-
-  ws.end();
 }
