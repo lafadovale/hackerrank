0654c3a29c315d5e41408baeff86c7940f440e20
lafadovale <lafa.vale@gmail.com>
2023-06-09 03:21:30 -0300

    Solution isolated

--- timeConversion.js
+++ timeConversion.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'timeConversion' function below.
  *
@@ -56,16 +32,4 @@ function timeConversion(time) {
   }
 
   return `${hours}:${minutes}:${seconds}`;
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const s = readLine();
-
-  const result = timeConversion(s);
-
-  ws.write(result + "\n");
-
-  ws.end();
 }
