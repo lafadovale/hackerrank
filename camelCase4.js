0a103ef9e1c113d17f41fa8086b7c87ecfcdaf9f
Lafaiete do Vale <lafa.vale@gmail.com>
2023-07-01 23:55:31 -0300

    Solution isolated

--- camelCase4.js
+++ camelCase4.js
@@ -54,14 +54,3 @@ function processData(input) {
     }
   }
 }
-
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-  _input += input;
-});
-
-process.stdin.on("end", function () {
-  processData(_input);
-});
